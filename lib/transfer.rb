e0ab7bb97cf2c8fa87e421b82c57759b4ba83470
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-25 18:37:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,20 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :sender, :receiver, :amount, :status
+  
+  def initialize(sender, receiver, amount, status = "pending")
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = status
+  end
+  
+  def valid?
+    @sender.valid? && @receiver.valid?
+  end 
+  
+  def execute_transaction
+    
+  end
+  
 end
