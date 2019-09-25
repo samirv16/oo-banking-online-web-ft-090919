35af655481acea272debb6ffc935ac36dc0a0c4b
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-25 19:53:38 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,7 +22,6 @@ class Transfer
       @receiver.balance += @amount
       @status = "complete"
     else
-      
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
@@ -30,17 +29,4 @@ class Transfer
   
 end
 
-# def execute_transaction
-#     if @status == "complete"
-#       puts "This transaction has already been completed."
-#     end 
-#     if valid? && @sender.balance > @amount && @status == "pending" 
-#       @sender.balance -= @amount 
-#       @receiver.deposit(@amount)
-#       @status = "complete"
-#     else   
-#       return "Transaction rejected. Please check your account balance."
-#       @status = "rejected"
-#     end 
-#   end
 
