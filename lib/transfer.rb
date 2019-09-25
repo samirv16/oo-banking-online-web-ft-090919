afe21206f43826c5c382efe76d4bc682c799b8a3
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-25 19:18:56 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,10 +18,9 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-      
     else 
       @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
+      puts "Transaction rejected. Please check your account balance."
     end
   end
   
