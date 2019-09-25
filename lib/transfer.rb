88efdd38da869f0e7f48e1b014dc8a49c993e268
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-25 19:35:34 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,20 +10,17 @@ class Transfer
   end
   
   def valid?
-    @sender.valid? && @receiver.valid? 
+    @sender.valid? && @receiver.valid? ? true : false 
   end 
   
   def execute_transaction
-    if @sender.balance < @amount
+    if @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
-
-    elsif @status == "complete"
-      puts "Transaction was already excuted"
-    else
-      @sender.deposit( @amount * -1 ) 
-      @receiver.deposit( @amount )
-      @status = "complete"
     end
   end
   
